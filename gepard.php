5e48b5f1cad5cc150cf1374902fbc5ec1b9d87e7
html prgrmmr <88141488+jamaalisch@users.noreply.github.com>
2022-12-01 23:03:53 +0100

    Add files via upload

---
+++ gepard.php
@@ -0,0 +1,56 @@
+<?php require("functions.php");
+    tplHead("Der Gepard", "panzer");
+    tplHeader("gepard", "Der Gepard");
+?>
+
+<main>
+
+    <div class="container">
+
+        <div class="panzer">
+            <img src="/images/Gepard.jpg" alt="Gepard">
+        </div>
+
+        <div class="grid-cols-3">
+
+            <div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d146829.2090245991!2d8.242988215968916!3d54.899299618778535!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b4dc56b4abc3db%3A0x89b986bbf2301146!2sSylt!5e0!3m2!1sde!2sde!4v1669926651645!5m2!1sde!2sde" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+
+            <div class="content">
+                    <p>Der Flugabwehrkanonenpanzer Gepard (FlakPz Gepard) ist ein autonomer, allwetterkampffähiger FlaK-Panzer aus deutscher Produktion. Bereits in den 1970er-Jahren entwickelt und produziert, bildete er für lange Zeit einen Eckpfeiler der Flugabwehr des Heeres der Bundeswehr, des niederländischen und des belgischen Heeres. Mit der Außerdienststellung bei den ursprünglichen Nutzern Ende der 1990er- bis Anfang der 2000er-Jahre wird er in der Zwischenzeit nur noch bei anderen Armeen verwendet.</p>
+                    <br>
+                    <h2>Informationen</h2> <br>
+                    <ul>
+                    <li>Hersteller: MTU</li>
+                    <li>Länge: 7,86 Meter</li>
+                    <li>Breite: 3,27 Meter</li>
+                    <li>Höhe: 3,29 Meter</li>
+                    <li>Reichweite: 550 km </li>
+                    <li>Höchstgeschwindigkeit: 65 km/h</li>
+                    <li>Federung: Drehstabfederung </li>
+                    <li>Besatung: 3 Personen</li>
+                    <li>Hauptbewaffung: 2 × 35-mm-L/90-Maschinenkanonen Oerlikon-KD</li>
+                    <li>Motor: 10-Zylinder-37,4-Liter-Vielstoffmotor MTU MB 838 CaM-500</li>
+                    <li>Leistung: 830 PS</li>
+                    <li>Verbrauch: 150 l / 100 km </li>
+                    <li>Tankvolumen: 985 l </li>
+                    <li>Gewicht: 47,5 Tonnen</li>
+                    </ul>
+            </div>
+
+            <div class="leasing">
+                <a href="https://www.youtube.com/watch?v=r8OH8Mq8NMw">Leasing</a>
+            </div>
+
+        </div>
+
+    </div>
+
+</main>
+
+<?php tplFooter(); ?>
+
+
+
+
---
+++ m1ax.php
@@ -0,0 +1,45 @@
+<?php require("functions.php");
+    tplHead("Der M1 AbramsX", "panzer");
+    tplHeader("m1ax", "Der M1 Abrams");
+?>
+
+<main>
+
+    <div class="container">
+
+        <div class="panzer">
+            <img src="/images/m1ax.png" alt="M1 AbramsX">
+        </div>
+
+        <div class="grid-cols-3">
+
+            <div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d158739.03418716174!2d-83.14477419782192!3d42.3299999162229!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8824ca0110cb1d75%3A0x5776864e35b9c4d2!2sDetroit%2C%20Michigan%2C%20USA!5e0!3m2!1sde!2sde!4v1669926369036!5m2!1sde!2sde" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+
+            <div class="content">
+                
+            <p>M1 AbramsX Tech Demonstrator <br> 
+
+        Der "AbramsX" ist ein Technologie-Testbed basierend auf dem M1 Abrams, dem Haupt-Gefechtspanzer der USA. Nach 40 Jahren aktivem Dienst überlegt die US Army nun, ein allumfassendes Upgrade durchzuführen.
+        Statt der unter deutschen Lizenz produzierten RM256 120mm L/44 Bordkanone verwendet AbramsX die leichtere XM360. Zudem wurden sekundäre Bordwaffen durch die von Kongsberg produzierte RS6-Mehrzweckkontrollstation ersetzt, die den oberen Teil des Turms beschmückt. Diese Waffenstation umfasst neben einer Sensor Suite ebenfalls ein 7.62mm Maschinengewehr und die XM914 30mm Autokanone.
+        Um den Kampfwert weiter zu steigern, verfügt AbramsX ebenfalls über sechs Kamikaze-Drohnen, die Switchblade 300, welche vom Panzerkommandanten ferngesteuert werden und effektiv leichte Fahrzeuge, Kampfstellungen und Infanterie bekämpfen können.
+        Eine der entscheidensten Neuerungen, die AbramsX austesten wird, ist das Crewless Turret. Das bedeuted, dass sich Besatzung in einer Kapsel vorne in der Hülle befinden, während sie über fortgeschrittene Elektronik die Bewaffnung fernsteuern. Dies schützt die Crew um einiges besser vor Beschuss und durchschlagenden Wuchtgeschossen. Dafür hat die XM360 120mm ebenfalls einen Autolademechanismus erhalten.
+        General Dynamics betont bei diesem Modell, dass es sich nicht um den nächsten Panzer der US Army handelt. Stattdessen ist dies ein Blick in die Zukunft westlicher Panzertechnologie, vor denen sich chinesische ZTZ-Panzer und der russische (und abgebrochene) T-14 Armata in Acht nehmen müssen.</p>
+            </div>
+
+            <div class="leasing">
+                <a href="https://www.youtube.com/watch?v=EYbvhWEG6kE">Leasing für 229€ p.a.</a>
+            </div>
+
+        </div>
+
+    </div>
+
+</main>
+
+<?php tplFooter(); ?>
+
+
+
+
